e95f97d1c563e15b98c5703604eda9080116792e
rederry <rederry@outlook.com>
2017-12-19 15:13:28 +0800

    Refactor concentration model

--- Concentrate/Concentration.swift
+++ Concentrate/Concentration.swift
@@ -12,82 +12,18 @@ struct Concentration {
     
     var cards = [Card]()
     
-    private var indexOfOnlyOneFaceUp : Int? {
-        get {
-            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
-            
-//            let faceUpIndices = cards.indices.filter { cards[$0].isFaceUp }
-//            return faceUpIndices.count == 1 ? faceUpIndices.first : nil
-            
-//            let filtedCards = cards.filter{ $0.isFaceUp }
-//            return (filtedCards.count == 1) ? cards.index(of: filtedCards.first!) : nil
-            
-//            var foundIndex :Int? // default nil
-//            for index in cards.indices {
-//                if cards[index].isFaceUp && foundIndex == nil {
-//                    foundIndex = index
-//                } else if cards[index].isFaceUp && foundIndex != nil {
-//                    return nil
-//                }
-//            }
-//            return foundIndex
-        }
-        set {
-            for index in cards.indices {
-                cards[index].isFaceUp = (index == newValue)
-            }
-        }
-    }
-    
-    private(set) var flipCount = 0
-    private(set) var score = 0
-    private var mismatchedCards = Set<Card>()
-    
-    mutating func startNewGame() {
-        for index in cards.indices {
-            cards[index].isFaceUp = false;
-            cards[index].isMatched = false;
-        }
-        shuffCards()
-        flipCount = 0
-        score = 0
-        mismatchedCards = Set<Card>()
-        lastMatchTime = Date()
-    }
-    
-    private var lastMatchTime = Date()
-    
     mutating func choseCard(at index: Int) {
         if !cards[index].isMatched, !cards[index].isFaceUp {
             if let matchIndex = indexOfOnlyOneFaceUp, index != matchIndex{
                 // match with the only one face up card
                 if cards[index] == cards[matchIndex] {
-                    // matched
+                    // matched update score
                     cards[index].isMatched = true
                     cards[matchIndex].isMatched = true
-                    let time = Date().timeIntervalSince(lastMatchTime)
-                    var bonus = 0
-                    switch time {
-                    case 0..<3:
-                         bonus = 2
-                    case 3..<5:
-                         bonus = 1
-                    case 5..<10:
-                         bonus = 0
-                    default:
-                         bonus = -1
-                    }
-                    score += 2 + bonus
-                    lastMatchTime = Date()
-                } else { // mismatch update score
-                    if (mismatchedCards.contains(cards[index])) {
-                        score -= 1
-                    }
-                    if (mismatchedCards.contains(cards[matchIndex])) {
-                        score -= 1
-                    }
-                    mismatchedCards.insert(cards[index])
-                    mismatchedCards.insert(cards[matchIndex])
+                    incScore()
+                } else {
+                    // mismatch update score
+                    decScore(index, matchIndex)
                 }
                 cards[index].isFaceUp = true
             } else {
@@ -104,6 +40,57 @@ struct Concentration {
             cards += [newCard, newCard] // Copy by value(value type) then a pair of card have the same identifier
         }
         shuffCards()
+    }
+    
+    private var indexOfOnlyOneFaceUp : Int? {
+        get { return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly }
+        set { cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue) } }
+    }
+    
+    private(set) var flipCount = 0
+    private(set) var score = 0
+    private var mismatchedCards = Set<Card>()
+    
+    mutating func startNewGame() {
+        cards.indices.forEach {
+            cards[$0].isFaceUp = false
+            cards[$0].isMatched = false
+        }
+        shuffCards()
+        flipCount = 0
+        score = 0
+        mismatchedCards = Set<Card>()
+        lastMatchTime = Date()
+    }
+    
+    private var lastMatchTime = Date()
+    
+    private mutating func incScore() {
+        let time = Date().timeIntervalSince(lastMatchTime)
+        var bonus = 0
+        switch time {
+        case 0..<3:
+            bonus = 2
+        case 3..<5:
+            bonus = 1
+        case 5..<10:
+            bonus = 0
+        default:
+            bonus = -1
+        }
+        score += 2 + bonus
+        lastMatchTime = Date()
+    }
+    
+    private mutating func decScore(_ index: Int, _ matchIndex: Int) {
+        if (mismatchedCards.contains(cards[index])) {
+            score -= 1
+        }
+        if (mismatchedCards.contains(cards[matchIndex])) {
+            score -= 1
+        }
+        mismatchedCards.insert(cards[index])
+        mismatchedCards.insert(cards[matchIndex])
     }
     
     private mutating func shuffCards() {
